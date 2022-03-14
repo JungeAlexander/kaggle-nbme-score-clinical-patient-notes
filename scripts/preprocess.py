c06b30b7c7f8e6edef935bd21f682a8564138110
Alexander Junge <alexander.junge@gmail.com>
2022-03-14 19:39:02 +0100

    First attempt to write standalone preprocessing script

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -12,18 +12,7 @@ from wasabi import msg
 
 random.seed(42)
 
-
-def main(
-    input_dir: Path = typer.Argument(..., exists=True),
-    output_dir: Path = typer.Argument(...),
-    case_num: int = typer.Argument(0),
-    feature_num: int = typer.Argument(0),
-    train_json_file_name: str = "train_split.json.gz",
-):
-    nlp = spacy.blank("en")
-
-    with gzip.open(input_dir / train_json_file_name, "r") as fin:
-        df = pd.read_json(fin)
+nlp = spacy.blank("en")
 
 
 def process_spans(span_str: str):
@@ -36,44 +25,15 @@ def process_spans(span_str: str):
     """
     spans = []
     for s in span_str:
-        current_s = []
         ints = re.findall(r"\d+", s)
         assert len(ints) % 2 == 0
-        for i in range(0, len(ints), 2):
-            current_s.append((int(ints[i]), int(ints[i + 1])))
+        current_s = [(int(ints[i]), int(ints[i + 1])) for i in range(0, len(ints), 2)]
         spans.append(current_s)
     return spans
 
 
-df.location = df.location.apply(process_spans)
-
-
-for i, r in df.iterrows():
-    for expected, span in zip(r.annotation, r.location):
-        actual = ""
-        sep = ""
-        for sub in span:
-            actual += sep + r.pn_history[sub[0] : sub[1]]
-            sep = " "
-        if expected != actual:
-            print(f"Row: {i}")
-            print(f"Expected: {expected}")
-            print(f"Actual: {actual}")
-
-
-sub_df = df.loc[(df.case_num == case_num) & (df.feature_num == feature_num), :]
-
-
-feature_text = sub_df.feature_text.unique()
-assert len(feature_text) == 1
-feature_text[0]
-
-
-assert len(sub_df.pn_num.unique()) == len(sub_df)
-
-
-def df_to_docbin(in_df: pd.DataFrame):
-    db = DocBin()
+def df_to_doc_list(in_df: pd.DataFrame):
+    doc_list = []
     for _, row in in_df.iterrows():
         doc = nlp(row.pn_history)
         doc.user_data = {
@@ -92,38 +52,82 @@ def df_to_docbin(in_df: pd.DataFrame):
                     raise ValueError()
                 ents.append(char_span)
         doc.ents = ents
-        db.add(doc)
-    return db
+        doc_list.append(doc)
+    return doc_list
 
 
-train_df = sub_df.loc[
-    sub_df.fold.isin([0, 1, 2]),
-]
-train_db = df_to_docbin(train_df)
+def main(
+    input_dir: Path = typer.Argument(..., exists=True),
+    output_dir: Path = typer.Argument(...),
+    case_num: int = typer.Argument(0),
+    feature_num: int = typer.Argument(0),
+    train_json_file_name: str = "train_split.json.gz",
+):
+    msg.info("Loading raw data.")
+    with gzip.open(input_dir / train_json_file_name, "r") as fin:
+        df = pd.read_json(fin)
 
-dev_df = sub_df.loc[
-    sub_df.fold.isin([3]),
-]
-dev_db = df_to_docbin(dev_df)
+    df.location = df.location.apply(process_spans)
 
-test_df = sub_df.loc[
-    sub_df.fold.isin([4]),
-]
-test_db = df_to_docbin(test_df)
+    for i, r in df.iterrows():
+        for expected, span in zip(r.annotation, r.location):
+            actual = ""
+            sep = ""
+            for sub in span:
+                actual += sep + r.pn_history[sub[0] : sub[1]]
+                sep = " "
+            if expected != actual:
+                msg.warn("Mismatched span: ")
+                msg.warn(f"Row: {i}")
+                msg.warn(f"Expected: {expected}")
+                msg.warn(f"Actual: {actual}")
+                msg.warn("")
 
+    sub_df = df.loc[(df.case_num == case_num) & (df.feature_num == feature_num), :]
 
-assert len(train_db) == 60
-assert len(dev_db) == 20
-assert len(test_db) == 20
+    feature_text = sub_df.feature_text.unique()
+    assert len(feature_text) == 1
+    feature_text[0]
 
+    assert len(sub_df.pn_num.unique()) == len(sub_df)
 
-train_db.to_disk(
-    output_dir / f"train-case_num-{case_num}-feature_num-{feature_num}.spacy"
-)
-dev_db.to_disk(output_dir / f"dev-case_num-{case_num}-feature_num-{feature_num}.spacy")
-test_db.to_disk(
-    output_dir / f"test-case_num-{case_num}-feature_num-{feature_num}.spacy"
-)
+    train_df = sub_df.loc[
+        sub_df.fold.isin([0, 1, 2]),
+    ]
+    train_doc_list = df_to_doc_list(train_df)
+
+    dev_df = sub_df.loc[
+        sub_df.fold.isin([3]),
+    ]
+    dev_doc_list = df_to_doc_list(dev_df)
+
+    test_df = sub_df.loc[
+        sub_df.fold.isin([4]),
+    ]
+    test_doc_list = df_to_doc_list(test_df)
+
+    msg.good(f"Num Train Docs: {len(train_doc_list)}")
+    msg.good(f"Num Dev Docs: {len(dev_doc_list)}")
+    msg.good(f"Num Test Docs: {len(test_doc_list)}")
+
+    random.shuffle(train_doc_list)
+
+    train_db = DocBin(docs=train_doc_list)
+    dev_db = DocBin(docs=dev_doc_list)
+    test_db = DocBin(docs=test_doc_list)
+
+    with msg.loading(f"Saving docs to: {output_dir}..."):
+        train_db.to_disk(
+            output_dir / f"train-case_num-{case_num}-feature_num-{feature_num}.spacy"
+        )
+        dev_db.to_disk(
+            output_dir / f"dev-case_num-{case_num}-feature_num-{feature_num}.spacy"
+        )
+        test_db.to_disk(
+            output_dir / f"test-case_num-{case_num}-feature_num-{feature_num}.spacy"
+        )
+        msg.good("Done.")
+
 
 if __name__ == "__main__":
     typer.run(main)
